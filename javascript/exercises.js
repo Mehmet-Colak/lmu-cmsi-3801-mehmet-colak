55c453ee13ada389020b0242a55752c2df31d678
Mehmet-Colak <90007631+Mehmet-Colak@users.noreply.github.com>
2024-09-09 23:39:57 -0700

    working on q3 js

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -16,10 +16,45 @@ export function change(amount) {
 }
 
 // Write your first then lower case function here
+export function firstThenLowerCase( words, predicate) {
+  for (let word of words) {
+    const lower = word.toLowerCase();
+    if (predicate?.(lower)) {
+      return lower;
+    }
+  }
+  return undefined;
+}
 
 // Write your powers generator here
+export function* powersGenerator({ ofBase: base, upTo: limit }){
+  let value = 1
+  while (value <= limit) {
+    yield value
+    value *= base
+  }
+}
 
 // Write your say function here
+export function say(message) {
+  if (message === undefined) {
+    return "";
+  }
+
+  function sayer(message) {
+    this.message = message;
+  }
+
+  sayer.prototype.call = function(next) {
+    if (typeof next === 'string' && typeof this.message === 'string') {
+      return new sayer(this.message + " " + next);
+    } else {
+      return this.message;
+    }
+  };
+
+  return new sayer(message);
+}
 
 // Write your line count function here
 
